ccaf385d49bfdd2ad951181c5ddb287a000b06ad
Wrenchy <36445140+teerameth@users.noreply.github.com>
2020-10-25 22:51:36 +0700

    Change Filter Method

--- ponyslayer/unicorn.py
+++ ponyslayer/unicorn.py
@@ -14,6 +14,18 @@ def rotate_image(image, angle):
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
     result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
     return result
+def padding_image(img, color):
+    s = max(img.shape[0:2])
+    f = np.ones((s, s, 3),np.uint8) * color
+    ax,ay = (s - img.shape[1])//2,(s - img.shape[0])//2 # Getting the centering position
+    f[ay:img.shape[0]+ay,ax:ax+img.shape[1]] = img
+    return f
+def padding_image_gray(img, color):
+    s = max(img.shape[0:2])
+    f = np.ones((s, s),np.uint8) * color
+    ax,ay = (s - img.shape[1])//2,(s - img.shape[0])//2 # Getting the centering position
+    f[ay:img.shape[0]+ay,ax:ax+img.shape[1]] = img
+    return f
 class ContourProcessor():
     def set_param(self, image_resolution, image_size, marker_size, marker_size_error, min_path_length, max_path_length, min_path_width, max_path_width):
         self.image_resolution = image_resolution
